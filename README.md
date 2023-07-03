81e3527b6140b1b0cd088666bee5d938afa218eb
Peng Li <peng.li@compass.com>
2023-07-03 16:48:09 +0800

    typescript-is-not-nil-for-array-filter-typing-demo

--- README.md
+++ README.md
@@ -1,18 +1,11 @@
-TypeScript Hello World Demo
+TypeScript "isNotNil" for array filtering typing Demo
 ===========================
+
+How to define a `isNotNil` function with type guide to simply the type casting in "array.filter"
+
+I recommend to make it a common util function since it's quite useful
 
 ```
 npm install
 npm run demo
 ```
-
-Note:
-
-`esModuleInterop` is recommended to set to `true`,
-since we can have consistent importing syntax with babel,
-always use:
-
-```
-import some from 'some'
-```
-
--- hello.ts
+++ hello.ts
@@ -1,1 +1,15 @@
-console.log('Hello');
+function isNotNil<T>(obj: T | null | undefined): obj is T {
+    return obj !== null && obj !== undefined
+}
+
+const list = ["abc", null, "ddd", undefined]
+
+
+const list2: string [] = list
+    // We have to as typing casting "as string[]", otherwise will get compilation error: Type '(string | null | undefined)[]' is not assignable to type 'string[]'.
+    .filter(it => it !== null && it !== undefined) as string[]
+console.log("list2:", list2)
+
+// NO compilation error
+const list3: string[] = list.filter(isNotNil)
+console.log("list3:", list3)
